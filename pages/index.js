fe4bdb7c898b337e8406fea9ba825de23c0c12da
yuto <h.yyt9n@gmail.com>
2022-09-30 08:25:46 +0900

    Blog fields & display blogs

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,21 @@ export default function Home({blogs}) {
     <PageLayout>
         <AuthorIntro/>
         <hr/>
-        {JSON.stringify(blogs)}
+        {/* {JSON.stringify(blogs)} */}
         <Row className="mb-5">
-          <Col md="10">
+          {/* <Col md="10">
             <CardListItem />
-          </Col>
+          </Col> */}
+          { blogs.map(blog =>
+            <Col key={blog.slug} md="4">
+              <CardItem
+                title={blog.title}
+                subtitle={blog.subtitle}
+              />
+            </Col>
+            )
+          }
 
-          <Col md="4">
-            <CardItem />
-          </Col>
         </Row>
     </PageLayout>
   )
