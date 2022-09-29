146b9999bc9cf35afc0316bc77957ed4360f01af
yuto <h.yyt9n@gmail.com>
2022-09-29 23:11:30 +0900

    connect sanity api to get blog contents

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,36 @@ import AuthorIntro from 'components/AuthorIntro';
 import CardItem from 'components/CardItem';
 import CardListItem from 'components/CardListItem';
 
-export default function Home() {
+import { getAllBlogs } from 'lib/api';
 
+export default function Home({blogs}) {
+  // debugger
   return (
     <PageLayout>
         <AuthorIntro/>
         <hr/>
-          <Row className="mb-5">
-            <Col md="10">
-              <CardListItem />
-            </Col>
+        {JSON.stringify(blogs)}
+        <Row className="mb-5">
+          <Col md="10">
+            <CardListItem />
+          </Col>
 
-            <Col md="4">
-              <CardItem />
-            </Col>
-          </Row>
+          <Col md="4">
+            <CardItem />
+          </Col>
+        </Row>
     </PageLayout>
   )
+}
+
+// This function is called during the build (build time)
+// Provides props to your page
+// It will create static page
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs,
+    }
+  }
 }
