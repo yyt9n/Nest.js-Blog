665a4bd9e95f6da9d7d62c3ad675dac3ca08943b
yyt9n <h.yyt9n@gmail.com>
2022-10-03 23:31:29 +0900

    change google fonts and link color

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,8 +1,13 @@
 import { Container } from 'react-bootstrap';
+import  Head  from 'next/head';
 import Navbar from 'components/Navbar';
 
 export default function PageLayout({children, className}) {
     return (
+        <>
+        <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
+        </Head>
         <Container>
             <Navbar />
 
@@ -13,11 +18,12 @@ export default function PageLayout({children, className}) {
 
             <footer className="page-footer">
                 <div>
-                <a href="#">courses</a>{' | '}
-                <a href="#">github</a>{' | '}
-                <a href="#">facebook</a>
+                    <a href="#">courses</a>{' | '}
+                    <a href="#">github</a>{' | '}
+                    <a href="#">facebook</a>
                 </div>
             </footer>
         </Container>
+        </>
     )
 }
