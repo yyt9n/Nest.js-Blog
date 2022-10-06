0726287263916edca081b216c3397485892ea46d
yyt9n <h.yyt9n@gmail.com>
2022-10-07 07:44:59 +0900

    Get blog data

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,13 +1,21 @@
 import PageLayout from 'components/PageLayout';
 import { useRouter } from 'next/router';
+import { getBlogBySlug } from 'lib/api';
 
-const BlogDetail = () => {
-    const { query } = useRouter();
+const BlogDetail = ({blog}) => {
     return (
         <PageLayout>
-            <h1>hello detail page - {query?.slug}</h1>
+            <h1>hello detail page - {blog?.slug}</h1>
         </PageLayout>
     )
+}
+
+export async function getServerSideProps({params}) {
+    const blog = await getBlogBySlug(params.slug);
+
+    return {
+        props: {blog},
+    }
 }
 
 export default BlogDetail;
