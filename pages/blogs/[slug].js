c2ffea7a28bb428cef43d80e030f2ff12ae81e9e
yyt9n <h.yyt9n@gmail.com>
2022-10-09 19:29:39 +0900

    Blog conten

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,7 +1,10 @@
 import PageLayout from 'components/PageLayout';
 import BlogHeader from 'components/BlogHeader';
 import { getAllBlogs, getBlogBySlug } from 'lib/api';
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap';
+import BlockContent from 'components/BlogContent';
+
+
 
 const BlogDetail = ({blog}) => {
     return (
@@ -15,9 +18,8 @@ const BlogDetail = ({blog}) => {
                 author={blog.author}
                 date={blog.date}
             />
-              <hr/>
-              {/* Blog Content Here */}
-              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            <hr/>
+            <BlockContent content={blog.content}/>
             </Col>
           </Row>
         </PageLayout>
